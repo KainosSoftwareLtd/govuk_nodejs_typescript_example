19cd9ac8f042acb4d26111dbd9a5c5ef52e449b2
Steven Alexander <s.alexander@kainos.com>
2018-05-09 09:51:35 +0100

    added browser tests

---
+++ tests/e2e/exampleFlowTest.js
@@ -0,0 +1,20 @@
+/* eslint-env mocha */
+/* global browser */
+
+describe('Example flow test', () => {
+  it('should allow submission on FormExample', () => {
+    return browser.url('/')
+
+      // Index
+      .waitForExist('#start')
+      .click('#start')
+
+      // FormExample
+      .waitForExist('#fullName')
+      .setValue('#fullName', 'Joe Bloggs')
+      .click('#save-and-continue')
+
+      // Index
+      .waitForExist('#start')
+  })
+})

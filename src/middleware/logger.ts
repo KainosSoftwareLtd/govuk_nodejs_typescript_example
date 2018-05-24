4fd3f360cdb98914305f98bb25994d9e1f2f5f68
Anne mcauley <A.mcauley@kainos.com>
2018-05-24 13:52:06 +0100

    tidy up code

--- src/middleware/logger.ts
+++ src/middleware/logger.ts
@@ -1,19 +1,15 @@
 import * as winston  from 'winston'
-import { json } from 'body-parser'
 import { get } from 'express-http-context'
-import { Stream } from 'stream'
 
 const level = process.env.LOG_LEVEL || 'debug'
-
-// tslint:disable-next-line:no-console
-console.log('inside logger.ts')
 
 const winstonLogger = new (winston.Logger) ({
     transports: [
         new (winston.transports.Console)({
             timestamp: function() {
                 return (new Date()).toISOString()
-            }
+            },
+            json: true,
         })
     ]
 })
